9f8b5ba2eeb09130f977bd11a4fca15523392989
ryanp <ryanpanimation@gmail.com>
2020-08-07 23:57:01 -0500

    create middleware

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
+
+module.exports = (req, res, next) => {
+  const [authType, token] = req.headers.authorization.split(" ");
+  // console.log("token: ", token);
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "invalid token" });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "no token included" });
+  }
+};
